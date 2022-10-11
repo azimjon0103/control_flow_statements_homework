dd913ec212ed8b8d8d849288064634ebd14c5933
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-11 23:50:02 +0500

    07

--- if07.py
+++ if07.py
@@ -13,14 +13,14 @@ def main(a):
         string: the message to print
     """
     if  a%2==1 and a>0:
-        sun="positive odd number "
+        sun="positive odd number"
     if  a%2==0 and a>0:
-        sun="positive even number "  
-    if abs(a)%2==1 and a<0:
-        sun="negative odd number " 
-    if  abs(a)%2==0 and a<0:
-        sun="negative even number " 
+        sun="positive even number"  
+    if  a%2==1 and a<0:
+        sun="negative odd number" 
+    if  a%2==0 and a<0:
+        sun="negative even number" 
     if  a==0:
         sun="the number is zero"             
     return sun
-print(main(-24))    
+print(main(67))    
