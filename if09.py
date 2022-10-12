a5bc2b393d3599fabc247e6ccd5413e3ade23d17
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-12 09:44:49 +0500

    09

--- if09.py
+++ if09.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 def main(a):
     """
     The two-digit integer is given.
@@ -9,4 +12,12 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    s1=a%10
+    s2=a//10
+    s=s1*10+s2*1
+    if a>=s:
+        x="True"
+    else :
+        x="False"    
+    return x
+print(main(21))    
