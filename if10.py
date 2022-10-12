43416fb40d1508303d8088be931fdeaca522a9bc
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-12 10:03:36 +0500

    10

--- if10.py
+++ if10.py
@@ -13,4 +13,17 @@ def main(temp):
     Returns:
         string: the message to print
     """
-    return
+    if temp<0:
+        sun="Freezing"
+    if temp>=1 and temp<=10  :
+        sun="Very Cold"
+    if  temp>=11 and temp<=20 :
+        sun= "Cold"
+    if  temp>=21 and temp<=30 :
+        sun="Normal"
+    if  temp>=31 and temp<=40 :
+        sun="Hot"  
+    if temp>40:
+        sun="Very Hot"   
+    return sun
+print(main(-8))    
