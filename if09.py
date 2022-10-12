9d171e202c128e2a5a0737dec2ca201a7ad63cc0
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-12 09:55:53 +0500

    09

--- if09.py
+++ if09.py
@@ -12,10 +12,10 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    s1=a%10
-    s2=a//10
-    s=s1*10+s2*1
-    if a>=s:
+    sun1=a%10
+    sun2=a//10
+    sun=sun1*10+sun2*1
+    if a>=sun:
         x="True"
     else :
         x="False"    
